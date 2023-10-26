335d844b1e044dfe9b7175e589f19ad0c5927945
GiftSibiya <giftsibiya.gb@gmail.com>
2023-10-26 22:59:01 +0200

    first commit

---
+++ taxi_math_flutter/lib/main.dart
@@ -0,0 +1,40 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const TaxiMath());
+}
+
+class TaxiMath extends StatelessWidget {
+  const TaxiMath({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Container(
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                  colors: [Colors.yellow, Colors.red])),
+          child: Center(
+            child: Container(
+              height: 450,
+              width: 300,
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.all(Radius.circular(10))),
+              child: Container(
+                child: Icon(Icons.menu),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
